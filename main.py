88e19f42c5267a6c55141a4be7cd923aabed807d
Karen Dantas <94451993+karendantas@users.noreply.github.com>
2023-07-19 08:57:45 -0300

    coisas funcionando agora

--- main.py
+++ main.py
@@ -3,25 +3,23 @@ import json
 #função que adciona um contato no dicionario
 def adicionar_contato ():
     contatos={}
-    resposta ="s"
-    with open('Agenda telefonica.txt','a') as arquivopy:
+    resposta = "s"
+    while (resposta != "n"):
+        with open('Agenda telefonica.txt','a') as arquivopy:
+        
+            nome= str(input("Digite seu nome: "))
+            telefone = int(input("Digite seu telefone: "))
+            contatos["nome"]=nome
+            contatos["telefone"]=telefone
+                
+            #for chaves, valor in contatos.items():
+                #print(chaves,valor)
 
-       while (resposta.lower() == "s"):
-         nome= str(input("Digite seu nome: "))
-         telefone = int(input("Digite seu telefone: "))
-         contatos[nome]= {
-            "nome" :nome,
-            "telefone" :telefone
-         }
-         for chaves, valor in contatos.items():
-            print(chaves,valor)
-           #for n in contatos.values():
-          #arquivopy.write(str(n)+"\n")
-       
-
-         arquivopy.write(str(contatos)+"\n")
-         #print("O contato de {} e seu numero: {}, foram adicionados a agenda!".format(contatos["nome"], contatos["telefone"]))
-         resposta = str(input("Adicionar outro contato?[s/n]"))
+            for n in contatos.values():
+                arquivopy.write(str(n)+"\n") #dessa forma o n pega os valores e os escreve na um embaixo do outro no arq
+        resposta = str(input("Adicionar outro contato?[s/n]"))       
+    return print("O contato de {} e seu numero: {}, foram adicionados a agenda!".format(contatos["nome"], contatos["telefone"]))
+           
         
          
 
@@ -34,23 +32,50 @@ def ler_agenda():
         for lines in linhas:
          print(lines)
     
+
+#função que pega as linhas do arquivo o transforma em um dicionario
+def pegar_linhas(dict):
+    with open ("Agenda telefonica.txt", "r") as arquipy:
+        linhas = arquipy.readlines()
+        contlinhas=0
+        for lines in linhas:
+            if (contlinhas %2 == 0):
+                dict["nome"]=linhas
+            elif (contlinhas %2 != 0):
+                dict["telefone"]=linhas
+            contlinhas =contlinhas+1
+        for n in dict.items():
+            print(n)
+
+
+def procurar_numero():
+    contatos={}
+    pegar_linhas(contatos)
+    procura = str(input("Digite seu nome: "))
+    if procura in contatos.values():
+        print("Contato de {}, foi encontrado!").format([procura])
+
 #MENU e criação da agenda.txt
 with open('Agenda telefonica.txt','r') as arquivopy:
-    print("-------MENU------")
-    print("1- Adicionar contato")
-    print("2- Listar agenda")
-    print("3 - Procurar contato por número")
-    print("4 - Procurar contato por nome")
-    print("5 - Alterar contato")
-    print("6 - Excluir contato")
-    print("7 - Apagar agenda")
-    option =  (int(input("Digite a sua opção: ")))
-
+    contatos={}
+    pegar_linhas(contatos)
+    option=0
     while(option !=8):
+        print("-------MENU------")
+        print("1- Adicionar contato")
+        print("2- Listar agenda")
+        print("3 - Procurar contato por número")
+        print("4 - Procurar contato por nome")
+        print("5 - Alterar contato")
+        print("6 - Excluir contato")
+        print("7 - Apagar agenda")
+        option =  (int(input("Digite a sua opção: ")))
         match (option):
             case 1:
                 adicionar_contato()
             case 2:
                 ler_agenda()
+            case 3:
+                procurar_numero()
             case 8:
                 print("Agenda fechando...")
